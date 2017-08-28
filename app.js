6ef7aff4a34cca89aab8903919f7191ffa79b105
DESKTOP-R3TBHPI\ducphan <itphanhoangduc@gmail.com>
2017-08-28 22:28:15 +0700

    add ejs template

--- app.js
+++ app.js
@@ -4,10 +4,15 @@ var express = require('express'),
     bodyParser = require("body-parser"),
     app = express();
 
+app.set("views", __dirname + "/apps/views");
+app.set("view engine", "ejs");
+
 app.use(controllers);
 
 // parse application/x-www-form-urlencoded 
 app.use(bodyParser.urlencoded({ extended: false }))
+
+
 
 app.listen(3000, function () {
     console.log("Server run at port", config.get("server.port"));
--- apps/controllers/index.js
+++ apps/controllers/index.js
@@ -4,7 +4,7 @@ var route = express.Router();
 route.use("/admin", require(__dirname + "/admin"));
 route.use("/blog", require(__dirname + "/blog"));
 route.get("/", function (req, res) {
-    res.json("this is home page");
+    res.render("test");
 })
 
 module.exports = route;
